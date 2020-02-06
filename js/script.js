93dd290296a1f1437e176244b94db896444a4627
mattjboland <mattjamesboland@gmail.com>
2020-02-06 18:07:38 +0000

    added .selection into style in table.html also added functions script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
-    $("tr:odd").addClass("odd");
-    $("tr:even").addClass("even");
+    $("th").click(function(){
+        $("tr").children().removeClass("selection")
+        $(this).siblings().addClass("selection");
+    });
 });
